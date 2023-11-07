7f561d93b923380f9311f8cef216c9c365daaca0
Janio1215 <janioltr@hotmail.com>
2023-11-06 22:13:44 -0300

    update

--- application/config/github_updater.php
+++ application/config/github_updater.php
@@ -5,17 +5,17 @@
 /**
 * The user name of the git hub user who owns the repo
 */
-$config['github_user'] = 'RamonSilva20';
+$config['github_user'] = 'Janio1215';
 
 /**
 * The repo on GitHub we will be updating from
 */
-$config['github_repo'] = 'mapos';
+$config['github_repo'] = 'mapos-diferente';
 
 /**
 * The branch to update from
 */
-$config['github_branch'] = 'master';
+$config['github_branch'] = 'main';
 
 /**
 * The current commit the files are on.
@@ -23,7 +23,7 @@ $config['github_branch'] = 'master';
 * NOTE: You should only need to set this initially it will be
 * automatically set by the library after subsequent updates.
 */
-$config['current_commit'] = '53d35385917658bab3e048622325429d993f00de';
+$config['current_commit'] = '2dafedac353f6bb825c6f0988ba994197e60fba5';
 
 /**
 * A list of files or folders to never perform an update on.
